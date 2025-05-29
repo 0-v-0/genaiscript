91dd84f02e38312e3d13dacb682dbe36da3c1383
Peli de Halleux <pelikhan@users.noreply.github.com>
2025-05-29 15:00:54 +0000

    more docs

--- packages/cli/src/githubaction.ts
+++ packages/cli/src/githubaction.ts
@@ -1,4 +1,6 @@
 export async function githubActionSetOutputs(res: GenerationOutput) {
+    if (!process.env.GITHUB_OUTPUT) return
+
     const { setOutput } = await import("@actions/core")
     setOutput("text", res.text || "")
     if (res.json) setOutput("data", JSON.stringify(res.json))
