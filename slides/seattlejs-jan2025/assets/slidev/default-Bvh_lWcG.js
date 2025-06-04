a87df1b34c5f80ec18e93505896cb78277b024fe
pelikhan <pelikhan@users.noreply.github.com>
2025-06-04 15:15:54 +0000

    Deploying to gh-pages from @ microsoft/genaiscript@f2746f7b5b23537d0525ba8b423c9729ae52b062 🚀

---
+++ slides/seattlejs-jan2025/assets/slidev/default-Bvh_lWcG.js
@@ -0,0 +1,1 @@
+import{f as o,o as t,j as s}from"../modules/vue-DcDMA6_y.js";import{_ as r}from"../index-DI0jhgsx.js";const c={},a={class:"slidev-layout default"};function n(e,l){return t(),o("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
