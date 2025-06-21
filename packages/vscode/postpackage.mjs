ca010527775d548419a52dd6945e0d3b3f4a8cf2
Peli de Halleux <pelikhan@users.noreply.github.com>
2025-06-21 14:35:05 +0000

    fix: update display name and dependencies in package.json and scripts

--- packages/vscode/postpackage.mjs
+++ packages/vscode/postpackage.mjs
@@ -4,5 +4,9 @@ const pkg = await fs.readJSON("./package.json")
 pkg.enabledApiProposals = pkg._enabledApiProposals
 pkg.displayName = "GenAIScript Insiders"
 delete pkg._enabledApiProposals
+pkg.dependencies["@genaiscript/core"] = pkg.version
+pkg.dependencies["@genaiscript/api"] = pkg.version
+pkg.dependencies["@genaiscript/runtime"] = pkg.version
 await fs.writeJSON("./package.json", pkg, { spaces: 4 })
 console.log(`cleaned package.json`)
+$`pnpm install`
--- packages/vscode/prepackage.mjs
+++ packages/vscode/prepackage.mjs
@@ -1,10 +1,12 @@
 import "zx/globals"
 
 const pkg = await fs.readJSON("./package.json")
-pkg._enabledApiProposals = pkg.enabledApiProposals
-pkg.displayName = "GenAIScript"
-delete pkg.enabledApiProposals
+pkg.dependencies["@genaiscript/core"] = pkg.version
+pkg.dependencies["@genaiscript/api"] = pkg.version
+pkg.dependencies["@genaiscript/runtime"] = pkg.version
 await fs.writeJSON("./package.json", pkg, { spaces: 4 })
 await fs.rm("genaiscript.vsix", { force: true })
 await fs.rm("genaiscript.manifest", { force: true })
 console.log(`cleaned package.json`)
+$`rm -Rf node_modules`
+$`npm install`
