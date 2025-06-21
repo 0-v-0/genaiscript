d272339658da6bba071375957086f65f6245977d
Peli de Halleux <pelikhan@users.noreply.github.com>
2025-06-21 15:08:23 +0000

    fix: correct directory navigation in npm publish script

--- scripts/npm-publish.mjs
+++ scripts/npm-publish.mjs
@@ -4,9 +4,10 @@ import "zx/globals"
 
 console.log("Publishing npm packages...")
 const otp = await question('Please enter your npm 2FA code: ')
+const cwd = process.cwd()
 for(const d of ["core", "api", "runtime", "cli"]) {
     console.log(`publishing ${d}`)
     cd(`packages/${d}`)
     await $`npm publish --access public --otp ${otp}`
-    cd(`..`)
+    cd(cwd)
 }
